a5b0d4201d991035b6ef54d81d4edf8ef3d35b5a
Adam Thomas <thomasadam83@hotmail.com>
2025-11-25 23:36:37 -0500

    Updated config

--- includes/config.php
+++ includes/config.php
@@ -5,5 +5,9 @@ $env = file(__DIR__.'/../.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 foreach($env as $value)
 {
   $value = explode('=', $value);  
+  
+  if($value[1] == 'false') $value[1] = false;
+  elseif($value[1] == 'true') $value[1] = true;
+
   define($value[0], $value[1]);
 }
